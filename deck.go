20087c1743b9420eea2b78adb39730cb7d9437e2
kemal can <kcsari@newmind.ai>
2024-05-25 22:23:36 +0300

    first commit

---
+++ deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type Deck []string
+type CardSuits []string
+type CardValues []string
+
+func getNewDeck() Deck {
+	cards := Deck{}
+	cardSuits := CardSuits{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := CardValues{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
+func (d Deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	card := getNewDeck()
+	card.print()
+}
